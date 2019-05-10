a36c781262d35742f3742a1be4dafd576a459a16
Christophe Galon <christophe.galon@pole-emploi.fr>
2019-05-10 15:26:35 +0200

    chore(doc): ajoute la doc des dailies à la page d'index générée

--- scripts/genere-la-doc.sh
+++ scripts/genere-la-doc.sh
@@ -11,8 +11,10 @@ node scripts/release-notes.js > target/generated-docs/changelog-cdv.json
 
 docker run --rm -v $PWD/src/doc/asciidoctor:/documents/ asciidoctor/docker-asciidoctor asciidoctor *.adoc
 docker run --rm -v $PWD/src/doc/asciidoctor:/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf *.adoc
+docker run --rm -v $PWD/src/doc/dailies:/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf *.adoc
 mv -f $PWD/src/doc/asciidoctor/*.html target/generated-docs/
 mv -f $PWD/src/doc/asciidoctor/*.pdf target/generated-docs/
+mv -f $PWD/src/doc/dailies/*.pdf target/generated-docs/
 cp $PWD/src/doc/asciidoctor/*.png target/generated-docs/
 
 cp $PWD/src/doc/index/index-test-spring-restdocs.adoc target/generated-docs/test-spring-restdocs
