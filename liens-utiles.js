cfdd9181dd25a1388671c8398a7b0870dcf6b5d8
Christophe Galon <christophe.galon@pole-emploi.fr>
2023-04-21 16:58:34 +0200

    feat(liens): crée une page de lien dynamique avec Vue.js

---
+++ liens-utiles.js
@@ -0,0 +1,45 @@
+function listeDesDonnees() {
+    let liens = [
+        {
+            theme: "Article généraliste",
+            url: "https://www.synolia.com/synolab/outils/living-documentation-pourquoi-ou-et-comment/",
+            description: "Résumé d'une conf sur la living doc."
+        },
+        {
+            theme: "Langage",
+            url: "https://docs.asciidoctor.org/asciidoctor/latest/",
+            description: "Documentation de référence Asciidoctor."
+        },
+        {
+            theme: "Langage",
+            url: "https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet",
+            description: "Cheatsheet Markdown."
+        },
+        {
+            theme: "Génération de documentation",
+            url: "https://www.sphinx-doc.org/en/master/",
+            description: "Génération de doc à partir de code Python."
+        },
+        {
+            theme: "Mise en forme de documentation",
+            url: "https://antora.org/",
+            description: "Outil de génération de sites documentaires."
+        },
+        {
+            theme: "Mise en forme de documentation",
+            url: "https://jekyllrb.com/",
+            description: "Générateur de site statique en Ruby."
+        },
+        {
+            theme: "Graphiques et diagrammes",
+            url: "https://mermaid.js.org/#/",
+            description: "Générateur de diagrammes. Simple et puissant."
+        },
+        {
+            theme: "Graphiques et diagrammes",
+            url: "https://www.chartjs.org/",
+            description: "Générateur de graphiques en JS. Opensource."
+        },
+    ]
+    return liens;
+}
