9407adba6134361d549ff5495d9b91ef6c595004
Christophe Galon <christophe.galon@pole-emploi.fr>
2023-04-21 17:34:09 +0200

    feat(liens): regroupe les liens par thème

--- liens-utiles.js
+++ liens-utiles.js
@@ -1,44 +1,61 @@
 function listeDesDonnees() {
     let liens = [
         {
-            theme: "Article généraliste",
-            url: "https://www.synolia.com/synolab/outils/living-documentation-pourquoi-ou-et-comment/",
-            description: "Résumé d'une conf sur la living doc."
+            titre: "Langage",
+            liste: [
+                {
+                    url: "https://docs.asciidoctor.org/asciidoctor/latest/",
+                    description: "Documentation de référence Asciidoctor."
+                },
+                {
+                    url: "https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet",
+                    description: "Cheatsheet Markdown."
+                },
+            ]
         },
         {
-            theme: "Langage",
-            url: "https://docs.asciidoctor.org/asciidoctor/latest/",
-            description: "Documentation de référence Asciidoctor."
+            titre: "Article généraliste",
+            liste: [
+                {
+                    url: "https://www.synolia.com/synolab/outils/living-documentation-pourquoi-ou-et-comment/",
+                    description: "Résumé d'une conf sur la living doc."
+                },
+            ]
         },
         {
-            theme: "Langage",
-            url: "https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet",
-            description: "Cheatsheet Markdown."
+            titre: "Génération de documentation",
+            liste: [
+                {
+                    url: "https://www.sphinx-doc.org/en/master/",
+                    description: "Génération de doc à partir de code Python."
+                }
+            ]
         },
         {
-            theme: "Génération de documentation",
-            url: "https://www.sphinx-doc.org/en/master/",
-            description: "Génération de doc à partir de code Python."
+            titre: "Mise en forme de documentation",
+            liste: [
+                {
+                    url: "https://antora.org/",
+                    description: "Outil de génération de sites documentaires."
+                },
+                {
+                    url: "https://jekyllrb.com/",
+                    description: "Générateur de site statique en Ruby."
+                },
+            ]
         },
         {
-            theme: "Mise en forme de documentation",
-            url: "https://antora.org/",
-            description: "Outil de génération de sites documentaires."
-        },
-        {
-            theme: "Mise en forme de documentation",
-            url: "https://jekyllrb.com/",
-            description: "Générateur de site statique en Ruby."
-        },
-        {
-            theme: "Graphiques et diagrammes",
-            url: "https://mermaid.js.org/#/",
-            description: "Générateur de diagrammes. Simple et puissant."
-        },
-        {
-            theme: "Graphiques et diagrammes",
-            url: "https://www.chartjs.org/",
-            description: "Générateur de graphiques en JS. Opensource."
+            titre: "Graphiques et diagrammes",
+            liste: [
+                {
+                    url: "https://mermaid.js.org/#/",
+                    description: "Générateur de diagrammes. Simple et puissant."
+                },
+                {
+                    url: "https://www.chartjs.org/",
+                    description: "Générateur de graphiques en JS. Opensource."
+                },
+            ]
         },
     ]
     return liens;
